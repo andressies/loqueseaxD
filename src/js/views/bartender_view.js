9e72bbac63a9181dfa3ba27a336fe33980244381
andres silva <huesose@gmail.com>
2019-05-16 21:59:35 +0000

    se termina vista del cliente, se modifica vista del admin

--- src/js/views/bartender_view.js
+++ src/js/views/bartender_view.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Navbar2 from "../component/navbar2";
+import ListadoPedidos from "../component/listadoPedidos";
+import Ordenes from "../component/ordenes";
 
-export class BartenderList extends React.Component {
+export class BartenderView extends React.Component {
 	render() {
 		return (
-	<nav>
-    <div class="nav-wrapper">
-      <a href="#!" class="brand-logo"><i class="material-icons">cloud</i>Logo</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="sass.html"><i class="material-icons">search</i></a></li>
-        <li><a href="badges.html"><i class="material-icons">view_module</i></a></li>
-        <li><a href="collapsible.html"><i class="material-icons">refresh</i></a></li>
-        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
-      </ul>
-    </div>
-  </nav>
-  );
+			<div className="row">
+				<div className="">
+					<Navbar2 />
+				</div>
+				<div className="row">
+					<div className="col s12 m4">
+						<ListadoPedidos />
+					</div>
+					<div className="col s12 m8">
+						<Ordenes />
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
